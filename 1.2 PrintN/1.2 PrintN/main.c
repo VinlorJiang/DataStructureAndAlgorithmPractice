3e07db2796289c35ed7c2e91ae699b1ff207234a
vinlor <13824380425@163.com>
2019-12-17 10:59:21 +0800

    data

---
+++ 1.2 PrintN/1.2 PrintN/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  1.2 PrintN
+//
+//  Created by SZDCMAC11 on 2019/12/5.
+//  Copyright © 2019 SZDCMAC11. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
